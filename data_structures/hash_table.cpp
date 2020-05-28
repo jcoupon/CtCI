26fb4ee5ae7a79da9439c6200faefec0a14bd8ab
Jean Coupon <jean.coupon@gmail.com>
2020-05-28 17:16:49 +0200

    some more tests

---
+++ data_structures/hash_table.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int y;
+
+void modify_y(){
+
+    y = 10;
+
+    return;
+}
+
+int main(){
+
+    cout << "Type a city" << endl;
+
+    char answer [20];  
+
+    cout << answer << endl;
+
+    return 0;
+
+}
+
+
