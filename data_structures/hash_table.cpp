06109cfc7a4dbdf075a54f7221c3e47f10741bd1
Jean Coupon <jean.coupon@gmail.com>
2020-05-29 17:47:48 +0200

    some more tests

--- data_structures/hash_table.cpp
+++ data_structures/hash_table.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int y;
 
+struct fruit_t{
+
+    string color;
+    double price;
+
+} ;
+
+
 void modify_y(){
 
     y = 10;
@@ -11,33 +19,75 @@ void modify_y(){
     return;
 }
 
+void change_color(fruit_t *fruit){
+
+
+    fruit->color = "red";
+
+    cout << (*fruit).color << (*fruit).price << endl;
+
+    return;
+}
+
+
+class Node {
+
+    double x, y;
+
+    public:
+        void set_x(double xx){x = xx;};
+        double get_x(){return x;};
+
+
+};
+
 int main(){
 
-    cout << "Type a city" << endl;
+    Node node;
 
-    // char answer [20];
+    node.set_x(10.);
 
-    string answer;
+    cout << node.get_x() << endl; 
 
-    // getline(cin, answer);
-    answer = "Geneve";
 
-    string other_variable = "this";
+    return 0;
 
-    string *p;
+    enum color_t {black, red, colour_bizarre};
 
-    p = &answer;
 
-    // cin >> answer;
+    cout << colour_bizarre << endl;
 
-    //string answer_string;
-    //answer_string = answer;
+    return 0;
 
-    //cout << answer_string << endl;
-    //cout << answer_string.c_str() << endl;
+    fruit_t orange;
 
-    cout << *p << p << endl;
+    orange.color = "orange";
+    orange.price = 10.0;
 
+    change_color(&orange);
+
+    cout << orange.color << orange.price << endl;
+
+
+    return 0;
+
+    cout << "Enter alloc size" << endl;
+  
+
+
+    long N;
+
+    cin >> N;
+
+    char * a = new char [N];
+
+    for (long i =0; i < N; i++){
+        a[i] = 'a';
+    }
+
+    cout << a[N-1] << endl;
+
+    cin >> N;
 
 
     return 0;
