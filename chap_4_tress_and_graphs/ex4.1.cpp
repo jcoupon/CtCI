7ca3d636591cd1be9bcc999ccde7858ef7769280
Jean Coupon <jean.coupon@gmail.com>
2020-06-07 15:31:47 +0200

    ex 3.1

---
+++ chap_4_tress_and_graphs/ex4.1.cpp
@@ -0,0 +1,42 @@
+/*
+Done well:
+
+To improve:
+ 
+ 
+*/
+
+#include <iostream>
+#include <string>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        std::vector<Node *> children;
+        bool marked;
+};
+
+void build_graph(){
+
+    Node *one = new Node();
+    Node *two = new Node();
+    Node *three = new Node();
+    Node *four = new Node();
+    Node *five = new Node();
+
+    
+
+
+}
+
+int main(){
+
+
+
+    return 0;
+}
+
