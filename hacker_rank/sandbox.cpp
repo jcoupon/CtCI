3e6132efbee350cda3f1bfce55449867aa95cdeb
Jean Coupon <jean.coupon@gmail.com>
2020-06-20 18:15:32 +0200

    sandbox

--- hacker_rank/sandbox.cpp
+++ hacker_rank/sandbox.cpp
@@ -9,19 +9,70 @@
 
 using namespace std;
 
+string countAndSay(int n) {
+    string s = "1";
+
+    for (int j=1; j<n; j++){
+
+        int count = 0;
+        string result ="";
+
+        for(int i=0; i<s.length(); i++){
+
+            count++;
+            if(i==s.length()-1){
+                result += to_string(count) + s[i];
+            }
+            if(i<s.length()-1 && s[i] != s[i+1]){
+                result += to_string(count) + s[i];
+                count = 0;
+            }
+        }
+
+        cout << result << endl;
+
+        s = result;
+
+    }
+
+    return s;
+}
+
+
+string countAndSayR(int n) {
+   
+    if (n==1) return "1";
+    else{
+
+        string s = countAndSayR(n-1);
+
+        int count = 0;
+        string result ="";
+
+        for(int i=0; i<s.length(); i++){
+
+            count++;
+            if(i==s.length()-1){
+                result += to_string(count) + s[i];
+            }
+            if(i<s.length()-1 && s[i] != s[i+1]){
+                result += to_string(count) + s[i];
+                count = 0;
+            }
+        }
+        return result;
+    }
+
+
+}
+
+
+
 int main(){
 
+    string result;
 
-
-    int result;
-    
-    try{
-        result = stoi("333456168168168168168684684684684");
-    }
-    catch(...){
-
-        result = 0;
-    }
+    result = countAndSayR(8);
 
     cout << result << endl;
 
