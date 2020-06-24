e23e22b5d708fa57e42744a236c6591322c84fa6
Jean Coupon <jean.coupon@gmail.com>
2020-06-24 18:39:11 +0200

    sandbox

--- hacker_rank/sandbox.cpp
+++ hacker_rank/sandbox.cpp
@@ -6,27 +6,81 @@
 #include <iterator>
 #include <map>
 #include <unordered_map>
+#include <algorithm>  
 
 using namespace std;
 
+int CLS(vector<int>& A, vector<int>& B, int i, int j, vector<vector<int>> memo){
 
-unordered_map<int, vector<int>>int_to_indices(vector<int>& vec){
 
-  unordered_map<int, vector<int>> result;
+    if(i<0 || j<0) return 0;
 
-  for(int i=0; i<vec.size(); i++){
-    result[vec[i]].push_back(i); 
-  }
-  
-  return result;
+    if (memo[i][j] != -1){
+        return memo[i][j];
+    }
+
+    int result;
+
+    if(A[i] == B[j]){
+        result = 1+CLS(A, B, i-1, j-1, memo);
+    }
+    else{
+
+        result = max(CLS(A, B, i-1, j, memo), CLS(A, B, i, j-1, memo));
+    }
+
+    memo[i][j] = result;
+
+    return result;
 }
 
+
+int CLS_DP(vector<int>& A, vector<int>& B){
+
+    int M = A.size();
+    int N = B.size();
+
+    vector<vector<int>> DP(M+1, vector<int>(N+1, 0));
+
+    for (int i=1; i<=M; i++){
+        for (int j=1; j<=N; j++){
+            if(A[i-1] == B[j-1]){
+                DP[i][j] += 1+DP[i-1][j-1];
+            }
+            else{
+                DP[i][j] = max(DP[i-1][j], DP[i][j-1]);
+            }
+        }
+    }
+    
+    return DP[M][N];
+
+}
+
+
+int maxUncrossedLines(vector<int>& A, vector<int>& B) {
+
+    int M = A.size();
+    int N = B.size();
+
+    vector<vector<int>> memo(M, vector<int>(N, -1));
+   
+    // return CLS(A, B, A.size()-1, B.size()-1, memo);
+
+    return CLS_DP(A, B);
+
+}
+  
+  
+    
+        
 int main(){
 
-    string result;
+    vector<int> A {2, 5, 1, 2, 5};
 
+    vector<int> B {10,5, 2, 1, 5, 2};
 
-    cout << result << endl;
+    cout << maxUncrossedLines(A, B) << endl;
 
 
     return 0;
