5340d13ef45c2bfc4b72252f6155dc56899f91c9
Jean Coupon <jean.coupon@gmail.com>
2020-06-28 15:31:08 +0200

    ex 1.3

---
+++ chap_2_linked_lists/ex2.3.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node{
+public:
+  int data;
+  Node *next;
+  Node (int data):  data(data), next(nullptr) {}
+
+};
+
+void deleteNode(Node *head, Node *nodeToDelete){
+  
+  Node *runner = head;
+
+  while(runner){
+  
+    if(runner->next == nodeToDelete){
+      runner->next = runner->next->next;
+      delete runner->next;
+      return;
+    }
+    else{
+      runner = runner->next;
+    }
+  }
+
+  return;
+}
+
+
+
+
+int main(){
+
+    Node *head = new Node(0);
+    vector<int> vec = {1, 3, 5, 1, 6};
+
+    Node *runner = head;
+    for(auto v:vec){
+        Node *node = new Node(v);
+        runner->next = node;
+        runner = node;
+    }
+
+
+
+
+    return 0;
+}
