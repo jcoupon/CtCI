d8c1aa048120598be39b7e5eaec42185003344c0
Jean Coupon <jean.coupon@gmail.com>
2021-01-24 16:50:06 +0100

    deleted binary

---
+++ chap_3_stacks_and_queues/ex3.4_Queues_via_Stack.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <stack>
+
+class MyQueue{
+    
+private:
+    std::stack<int> L;
+    std::stack<int> R;
+
+public:
+    // MyQueue(): default;
+
+    void push(int val){
+        L.push(val);
+    }
+
+    int front(){
+        if(R.empty()){
+            while(!L.empty()){
+                R.push(L.top());
+                L.pop();
+            }
+
+        }
+        if(R.empty())
+            return -1;
+        
+        int result = R.top();
+        R.pop();
+        return result;
+    }
+
+    bool isEmpty(){
+        return R.empty() && L.empty();
+    }
+};
+
+
+int main(){
+
+    MyQueue q;
+
+    for(int i=0; i<5; ++i) q.push(i+1);
+
+    q.front();
+    q.push(6);
+    q.front();
+    q.push(7);
+
+    while(!q.isEmpty()){
+        std::cout << q.front() << std::endl;
+    }
+    
+    return 0;
+
+
+
+}
