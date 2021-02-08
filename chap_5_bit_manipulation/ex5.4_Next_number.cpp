9a29473f9d77d114465a0d794be2fd88e7657d71
Jean Coupon <jean.coupon@gmail.com>
2021-02-08 21:44:13 +0100

    ex 5.4

--- chap_5_bit_manipulation/ex5.4_Next_number.cpp
+++ chap_5_bit_manipulation/ex5.4_Next_number.cpp
@@ -22,54 +22,85 @@ string toBinary(int n){
     return result;
 }
 
-pair<string, string> findNextPrev(string num){
 
-    pair<string, string> result = {"no next", "no prev"};
-    for(int i=num.size()-1; i>0; --i){
-        if(num[i] == '1' && num[i-1] == '0'){
-            result.first = num;
-            swap(result.first[i], result.first[i-1]);
-            break;
-        }
+int getNext(int n){
+
+  if(n==0 || n==INT_MAX)
+    return -1;
+
+  int c0 = 0;
+  int c1 = 0;
+  int tmp = n;
+  
+    while((tmp&1) == 0){
+        ++c0;
+        tmp >>= 1;
     }
-
-    for(int i=num.size()-2; i>=0; --i){
-        if(num[i] == '1' && num[i+1] == '0'){
-            result.second = num;
-            swap(result.second[i], result.second[i+1]);
-            break;
-        }
+    while((tmp&1) == 1) {
+        ++c1;
+        tmp >>= 1;
     }
+    int p = c0+c1;
 
-    return result;
+    if(c0+c1 == 31)
+        return -1;
+
+    n |= (1<<(p));
+    n &= ~((1<<p)-1);
+    n |= (1<<(c1-1))-1;
+
+    return n;
+
 }
+
+
+int getPrevious(int n){
+
+  if(n==0 || n==INT_MAX)
+    return -1;
+
+  int c0 = 0;
+  int c1 = 0;
+  int tmp = n;
+  
+    while((tmp&1) == 1){
+        ++c1;
+        tmp >>= 1;
+    }
+    while((tmp&1) == 0) {
+        ++c0;
+        tmp >>= 1;
+    }
+    if (tmp == 0) return -1;
+
+    int p = c0+c1;
+
+    n &= ((~0) << (p+1));
+    int mask = (1<<(c1+1)) - 1;
+    n |= mask << (c0-1);
+
+    return n;
+}
+
 
 int main(){
 
-    std::cout << toBinary(pow(2,15)) << std::endl;
+    int n;
 
-    pair<string, string> result;
+    n = 13948;
+    std::cout << toBinary(n) << ' ' << toBinary(getNext(n)) << ' ' <<  getNext(n) << std::endl;
 
-    // 0000000001111110, 0000000001111011
-    result = findNextPrev(toBinary(125));
-    std::cout << result.first << ' ' << result.second << std::endl;
+    n = INT_MAX-1;
+    std::cout << toBinary(n) << ' ' << toBinary(getNext(n)) << ' ' <<  getNext(n) << std::endl;
 
-    // 0000000000000010, no prev
-    result = findNextPrev(toBinary(1));
-    std::cout << result.first << ' ' << result.second << std::endl;
+    n = 10115;
+    std::cout << toBinary(n) << ' ' << toBinary(getPrevious(n)) << ' ' <<  getPrevious(n) << std::endl;
 
-    // no next, no prev
-    result = findNextPrev(toBinary(0));
-    std::cout << result.first << ' ' << result.second << std::endl;
+    n = 0;
+    std::cout << toBinary(n) << ' ' << toBinary(getPrevious(n)) << ' ' <<  getPrevious(n) << std::endl;
 
-    // no next, 0100000000000000
-    result = findNextPrev(toBinary(pow(2, 15)));
-    std::cout << result.first << ' ' << result.second << std::endl;
-
-    // no next, 0100000000000000
-    result = findNextPrev(toBinary(13948));
-    std::cout << toBinary(13948) << std::endl;
-    std::cout << result.first << ' ' << result.second << std::endl;
+    n = INT_MAX-1;
+    std::cout << toBinary(n) << ' ' << toBinary(getPrevious(n)) << ' ' <<  getPrevious(n) << std::endl;
 
     return EXIT_SUCCESS;
 }
